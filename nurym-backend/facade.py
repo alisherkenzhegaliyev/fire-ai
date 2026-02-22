b60a9bc500cd66e21201a57264043466217b74f0
Alisher Kenzhegaliyev <47297879+alisherkenzhegaliyev@users.noreply.github.com>
2026-02-22 10:37:05 +0500

    stable 2

--- nurym-backend/facade.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Session
-from application.register_user.register_user_application_service import UserRegistrationData
-from application.register_user.register_user_application_service import execute as execute_user_registration
-
-def register_user(params: UserRegistrationData, db: Session):
-    return execute_user_registration(params, db)
